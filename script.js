9af1330b6b6c9d6b6e013113d1f5a71b836ff278
IH NASRI <ih.nasri71@gmail.com>
2026-01-19 13:55:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-ScrollReveal().reveal('.phone-card', {
-  distance: '60px',
-  duration: 1000,
-  interval: 150,
-  origin: 'bottom'
-});
-
-ScrollReveal().reveal('.slider img', {
-  distance: '40px',
-  duration: 1200,
-  interval: 100,
-  origin: 'bottom'
+document.querySelectorAll('.shot-card').forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.cursor = 'pointer';
+  });
 });
