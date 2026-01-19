66aa6a56e4277574eb65a3a9be3ca43dc88d4035
IH NASRI <ih.nasri71@gmail.com>
2026-01-19 00:42:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+window.addEventListener("load", hideLoader);
+setTimeout(hideLoader, 3000);
 
+function hideLoader() {
+  const loader = document.getElementById("preloader");
+  if (loader) loader.style.display = "none";
+}
